8ef80913b4c96317de5a5ade7e2daf91419ef25c
Aloysius <aloysius.tan00@gmail.com>
2026-02-06 21:15:20 +0800

    Exercise 6

--- app.js
+++ app.js
@@ -99,3 +99,24 @@ myCar.model = 'Model Y';
 
 console.log('Original: ', anotherCar);
 console.log('Cloned: ', myCar);
+
+// ! Exercise 6:
+// a. Define a variable named `propertyName` and assign a string (like 
+//    'username', 'age', or 'email') to it.
+// 
+// b. Create an object named `userProfile`. 
+// 
+// c. Use `propertyName` as a dynamic key in `userProfile`. Assign it a 
+//    relevant value.
+//
+// d. Console log the `userProfile` object to see the result.
+//
+// Your code here:
+
+const propertyName = 'username';
+
+const userProfile = {
+    [propertyName]: 'aloyst1'
+}
+
+console.log(userProfile);
