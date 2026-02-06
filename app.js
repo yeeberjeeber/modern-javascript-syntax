9dc0325c1719c94d4c8848796beed3582391e836
Aloysius <aloysius.tan00@gmail.com>
2026-02-06 21:09:21 +0800

    Exercise 4

--- app.js
+++ app.js
@@ -57,3 +57,19 @@ const { make, model } = car;
 
 console.log(make);
 console.log(model);
+
+// ! Exercise 4: 
+// a. Duplicate the provided `morePizzaToppings` array using the spread 
+//    operator and assign it to a variable named `uncontroversialPizzaToppings`.
+// 
+// b. Console log the `uncontroversialPizzaToppings` variable.
+//
+// Starting code (don't modify this):
+
+const morePizzaToppings = ['Cheese', 'Sauce'];
+
+// Your code here:
+
+const uncontroversialPizzaToppings = [...morePizzaToppings];
+
+console.log(uncontroversialPizzaToppings);
